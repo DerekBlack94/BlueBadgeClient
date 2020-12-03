7bfa3396df9782e9a9d6943f736ebe24ae8fd756
Kelly Casper <kellyrcasper@gmail.com>
2020-12-02 19:41:35 -0500

    updated Signup.js to display error message re:unique requirement

--- src/Components/Auth/Login.js
+++ src/Components/Auth/Login.js
@@ -10,7 +10,7 @@ const Login = (props) => {
     const handleSubmit = (event) => {
         event.preventDefault();
 
-        fetch(`${APIURL}/user/login`, {
+        fetch('http://localhost:3000/user/login', {
 
             method: 'POST',
             body: JSON.stringify({user:{username: username, password: password}}),
