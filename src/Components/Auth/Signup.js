7d3d43338ac7cf8443ccd6d4564b3aebf908bcbd
Emory Brunner <emorybrunner@gmail.com>
2020-12-02 19:53:12 -0500

    fixing error

--- src/Components/Auth/Signup.js
+++ src/Components/Auth/Signup.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import {Form, FormGroup, Label, Input, Button} from 'reactstrap';
-// import APIURL from '../../helpers/environment'
+import APIURL from '../../helpers/environment'
 
 
 const Signup = (props) => {
